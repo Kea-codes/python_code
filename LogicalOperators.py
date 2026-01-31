9f4f7a5105a4ea2c59a9154f12df78a166fc8e03
KEAMOGETSE <Keamogetsemaremela29@gmail.com>
2026-02-01 00:08:21 +0200

    Modified The file by adding new code

--- LogicalOperators.py
+++ LogicalOperators.py
@@ -11,3 +11,16 @@ if Pass_Mark >= 50 and Student_Studied :
     print(f"The student got {Pass_Mark}% and its {Student_Studied} that He studied")
 elif Pass_Mark < 50 and Student_Studied :
     print(f"The student got {Pass_Mark}% and its {Student_Studied} that He studied")
+
+
+print()
+print("********************")
+print()
+
+temperature = float(input("Enter temperature: "))
+isRaining = True
+
+if temperature > 35 or temperature < 0 and isRaining:
+    print("The outdoor event is cancelled")
+else :
+    print("WE OUTSIDE!!!!")
